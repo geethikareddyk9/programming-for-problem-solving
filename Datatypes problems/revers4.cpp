bfc5302126905eff6a2d3109211273a4ab2bf16d
Sneha-Gunjari <83581856+Sneha-Gunjari@users.noreply.github.com>
2021-07-16 20:59:05 +0530

    Add files via upload

---
+++ Datatypes problems/revers4.cpp
@@ -0,0 +1,13 @@
+/* program to find reverse of given name */
+
+#include<stdio.h>
+int main()
+{
+	char a,b,c ;
+	printf("\nEnter a,b,c:");
+	scanf("%c %c %c",&c,&b,&a);
+	printf("%c %c %c",a,b,c);
+	return 0;
+	
+} 
+
