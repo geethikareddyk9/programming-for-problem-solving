83dbd522a51f78daecb961ae600ace17b7775e91
Sneha-Gunjari <83581856+Sneha-Gunjari@users.noreply.github.com>
2021-07-17 15:32:53 +0530

    Add files via upload

---
+++ Bit Wise operators/Toggle of A LSB of a number.cpp
@@ -0,0 +1,14 @@
+/*Program to toggle LSB of a number*/
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter the number:");
+	scanf("%d",&num);
+	if(num & 1 == 0)                              /*num is 0*/
+	printf("\n After toggle: %d",num|1);          
+	else                                          /*num is 1*/
+	printf("\n After toggle: %d",num^1);               
+	return 0;
+}
+
---
+++ Bit Wise operators/to set Lsb of a given integer.cpp
@@ -0,0 +1,11 @@
+/* Program to set LSB of a number*/
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter the number:");
+	scanf("%d",&num);
+	printf("The value of n after LSB is set is %d",num|1);
+	return 0;
+}
+
