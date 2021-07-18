41c81a743e61840759ac6092abf1436f495404b5
Sneha-Gunjari <83581856+Sneha-Gunjari@users.noreply.github.com>
2021-07-18 23:22:02 +0530

    Add files via upload

---
+++ Structures/size of structures.cpp
@@ -0,0 +1,19 @@
+/*programm to print thesize of the structures*/
+#include<stdio.h>
+struct sz
+{
+	int roll,marks;
+	char name[10];
+	
+};
+int main()
+{
+	int size;
+	struct sz s;
+	
+	size = sizeof(s);
+	printf("\nSize of the structure : %d",size);
+	return 0;
+	
+	
+}
