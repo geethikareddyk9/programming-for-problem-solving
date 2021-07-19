798f6afde0a3f7d9c644204b69500954fdcf7205
Sneha-Gunjari <83581856+Sneha-Gunjari@users.noreply.github.com>
2021-07-19 21:10:36 +0530

    Add files via upload

---
+++ Strings/multiline string.cpp
@@ -0,0 +1,19 @@
+/* Program to read multiple line strings*/
+
+#include<stdio.h>
+int main()
+{
+	int i;
+	char Name[100];
+	char ch='@';
+	printf("\nEnter a paragraph and end it with $ :\n");
+
+	for(i=0;ch!='$';i++)
+	{
+		ch = getchar();
+		Name[i]=ch;
+	}
+   Name[--i] = '\0';
+   printf("\n\n The string is \n");
+   puts(Name);
+}
