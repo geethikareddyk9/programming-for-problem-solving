7a86ff9b3fb483d08e28768a5855e56c6cb2d682
Sneha-Gunjari <83581856+Sneha-Gunjari@users.noreply.github.com>
2021-07-19 19:57:14 +0530

    Add files via upload

---
+++ Arrays/Max_Min_avg_Arrays.cpp
@@ -0,0 +1,25 @@
+/* Maximum and minimun and average of array*/
+
+
+#include <stdio.h>
+int main()
+{
+	int i,A[3]={9,5,4},max,lol=0;
+	for (i=1;i<3;i++)
+		/*if (A[i]>A[i+1])
+			max=A[i];
+		elif(A[i+1]>A[i])
+			max=A[i+1];
+		else*/
+		if (A[i]<A[lol])
+			lol=i;
+			
+		printf("%d %d",A[lol]);
+		
+	//for(i=0;i<)
+		return 0;
+	
+	// max,min,avg
+}
+
+
