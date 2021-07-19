297eb9c137ffda9123eb633fadaabbb0552ddf0c
Sneha-Gunjari <83581856+Sneha-Gunjari@users.noreply.github.com>
2021-07-19 20:59:03 +0530

    Add files via upload

---
+++ Conditional operators/even by conditional operators.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a,n;
+	printf("Enter two integres:");
+	
+	scanf(%d,&a,&n);
+	(a>=0);
+	? printf("/na=n%d",a)
+	: print("/nn=0%d,n");
+	retun 0;
+	
+	
+	
+	
+}
+	
+	
+
+
+
+
